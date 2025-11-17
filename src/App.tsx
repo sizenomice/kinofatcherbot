e75bac870ddb37c27578f441571d5c0e1be099de
sizenomice <sizenomice@icloud.com>
2025-11-17 11:36:58 +0300

    fix-build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  console.log(window?.Telegram?.WebApp);
+  console.log(window?.Telegram);
   return (
     <>
       <img src="/favicon.svg" alt="KinoFetcherBot" style={{ width: '100px', height: '100px' }} />
