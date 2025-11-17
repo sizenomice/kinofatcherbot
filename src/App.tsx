a820aa28684b3fd923708694dc8cbdf7bf4c6653
sizenomice <sizenomice@icloud.com>
2025-11-17 11:14:40 +0300

    init-kinofatcherbot

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <img src="/favicon.svg" alt="KinoFetcherBot" style={{ width: '100px', height: '100px' }} />
+      <div>KinoFetcherBot</div>
+    </>
+  )
+}
+
+export default App
