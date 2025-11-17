82cad1d8dbd79b202dea2d3e2c3122d9eb8145f2
sizenomice <sizenomice@icloud.com>
2025-11-17 11:19:34 +0300

    test_telegram_window

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
+  const alert = (window as any).telegram?.WebApp;
   return (
     <>
       <img src="/favicon.svg" alt="KinoFetcherBot" style={{ width: '100px', height: '100px' }} />
       <div>KinoFetcherBot</div>
+      <div>{alert}</div>
     </>
   )
 }
