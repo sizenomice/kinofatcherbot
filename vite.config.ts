11128c9c4c0d9dce0eeaad36c0eea88fcce9f878
sizenomice <sizenomice@icloud.com>
2025-11-17 18:15:01 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
+  },
 })
